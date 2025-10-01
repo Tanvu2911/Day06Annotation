f4b96338fad321ba951e4d2850848e7c71873f9e
Tanvu2911 <vuviettan29112005@gmail.com>
2025-10-01 20:19:28 +0700

    Cập nhật code

---
+++ Day06Lab/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day06Lab.Models;
+using System.Text.RegularExpressions;
+
+namespace Day06Lab.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static List<Account> accounts = new List<Account>();
+        public IActionResult Index()
+        {
+            return View(accounts);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Account model)
+        {
+            if (ModelState.IsValid)
+            {
+                accounts.Add(model); 
+                return RedirectToAction("Index");
+            }
+            //Account model = new Account();
+            return View(model);
+        }
+        [AcceptVerbs("GET","POST")]
+        public IActionResult VerifyPhone(string Phone)
+        {
+            Regex _isPhone = new Regex(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
+            if ( !_isPhone.IsMatch(Phone) )
+            {
+                return Json($"So din thoai {Phone} khong dung dinh dang , vd 0355880362 hoac 035.588.0362");
+            }
+            return Json(true);
+        }
+    }
+}
---
+++ Day06Lab/Models/Account.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day06Lab.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [
+            Display(Name ="Họ và tên"),
+            Required(ErrorMessage ="Họ tên không được để trống"),
+           StringLength(20,MinimumLength =6 , ErrorMessage =("họ tên từ 6 đến 20 ký tự"))
+        ]
+        public string FullName { get; set; }
+        [  
+            Display(Name = "Email"),
+            Required(ErrorMessage ="Email không được để trống"),
+            EmailAddress(ErrorMessage ="Email không đúng định dạng"),
+
+         ]
+        public string Email {  get; set; }
+        [
+            Display(Name ="Số điẹn thoại"),
+            Required(ErrorMessage = "Số diện thoại không được để trống"),
+            Phone(ErrorMessage ="Số điện thoại không hợp lệ"),
+            //RegularExpression(@"^(0[0-9]{9})$", ErrorMessage ="Số điện thoại phải đủ 10 số và bắt đầu bằng số không")
+            Remote(action: "VerifyPhone", controller:"Account")
+        ]
+
+        public string Phone {  get; set; }
+
+        [
+            Required(ErrorMessage ="Địa chỉ không được để trống"),
+            Display(Name = "Địa chỉ thường chú"),
+            StringLength(35, ErrorMessage ="Không được vượt quá 35 ký tự")
+            ]
+        public string Address {  get; set; }
+
+
+        [Display(Name = "Ảnh đại diện")]
+        public string Avatar { get; set; }
+        [
+            Display(Name ="Ngày sinh"),
+            Required(ErrorMessage ="Ngày sinh không được để trống"),
+            DataType(DataType.Date)
+            ]
+        public DateTime Birthday { get; set; }
+
+
+        [Display(Name = "Giới tính")]
+        public string Gender { get; set; }
+        [Display(Name = "Mật khẩu"),
+            DataType(DataType.Password),
+            ]
+
+        public string Password {  get; set; }
+        [Display(Name = "Facebook"),
+            Required(ErrorMessage ="Link Facebook không được để trống"),
+            Url(ErrorMessage ="URL phải đúng định dạng")]
+
+        public string Facebook { get; set; }
+
+    }
+}
